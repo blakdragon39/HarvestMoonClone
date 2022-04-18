6ca8abadd0f95f2842367ea8c5286ac2fae989d3
Blakdragon39 <jenna.hartley@live.ca>
2022-04-17 19:39:43 -0600

    Scrollwheel to select an inventory item

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -3,14 +3,37 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour {
     
-    private InventorySlot selectedSlot;
+    private Item selectedItem;
 
     [SerializeField] private List<InventorySlot> slots;
     [SerializeField] private List<Item> items;
 
     private void Start() {
         for (int i = 0; i < slots.Count && i < items.Count; i += 1) {
-            slots[i].SlotImage.sprite = items[i].UnselectedSprite;
+            slots[i].SetItem(items[i]);
+            slots[i].OnItemClick += OnItemSelected;
+        }
+    }
+
+    private void Update() {
+        var selectedItemIndex = items.IndexOf(selectedItem);
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        
+        if (scroll > 0) selectedItemIndex -= 1;
+        else if (scroll < 0) selectedItemIndex += 1;
+
+        if (scroll != 0) {
+            selectedItemIndex = Mathf.Clamp(selectedItemIndex, 0, items.Count - 1);
+            OnItemSelected(items[selectedItemIndex]);
+        }
+    }
+
+    private void OnItemSelected(Item item) {
+        selectedItem = item;
+
+        for (int i = 0; i < items.Count; i++) {
+            slots[i].SetSelected(slots[i].Item == item);
         }
     }
 }
--- Assets/Scripts/UI/InventorySlot.cs
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -1,9 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour {
 
+    public event Action<Item> OnItemClick;
+    public Item Item => item;
+    
     [SerializeField] private Image slotImage;
+    private Item item;
 
-    public Image SlotImage => slotImage;
+    public void SetItem(Item item) {
+        this.item = item;
+        slotImage.sprite = item.UnselectedSprite;
+    }
+
+    public void SetSelected(bool selected) {
+        slotImage.sprite = selected ? item.SelectedSprite : item.UnselectedSprite;
+    }
+    
+    private void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0)) {
+            OnItemClick(item);
+        }
+    }
 }
