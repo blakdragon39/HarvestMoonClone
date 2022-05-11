8ab821a0fb355515bd7b411c3bf3c4460992b6db
Blakdragon39 <jenna.hartley@live.ca>
2022-05-10 21:06:35 -0600

    Added a singular unfunctioning building.

---
+++ Assets/Tiled Assets/Wooden House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Wooden House" tilewidth="16" tileheight="16" tilecount="30" columns="6" objectalignment="center">
+ <image source="../Art/tilesets/Building parts/Wooden House.png" width="96" height="80"/>
+</tileset>
